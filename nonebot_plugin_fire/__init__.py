ce6f4b7f0407fa0dd0253f98a6e9fe5b1031fa0f
liuxiangyuliu_i <liuxiangyuliu_i@didiglobal.com>
2022-03-20 13:09:18 +0800

    fix

--- nonebot_plugin_fire/__init__.py
+++ nonebot_plugin_fire/__init__.py
@@ -1,17 +1,36 @@
 import asyncio
 import random
-
-import nonebot
-from nonebot import require, get_bot
+from nonebot import require, get_bot, get_driver
 from nonebot.log import logger
 
-scheduler = require("nonebot_plugin_apscheduler").scheduler
-fire_user_id = nonebot.get_driver().config.fire_user_id
+
+try:
+    scheduler = require("nonebot_plugin_apscheduler").scheduler
+except BaseException:
+    scheduler = None
+
+logger.opt(colors=True).info(
+    "已检测到软依赖<y>nonebot_plugin_apscheduler</y>, <g>开启定时任务功能</g>"
+    if scheduler
+    else "未检测到软依赖<y>nonebot_plugin_apscheduler</y>，<r>禁用定时任务功能</r>"
+)
+
+fire_user_id = get_driver().config.fire_user_id
 
 
-@scheduler.scheduled_job("cron", minute="*")
-async def run_every_second():
-    bot = get_bot()
-    await asyncio.sleep(random.randint(1, 10))
-    logger.info("发送火花")
-    await bot.call_api("send_msg", user_id=fire_user_id, message="/echo 花花")
+async def fire_scheduler():
+    sendSuccess = False
+    while not sendSuccess:
+        try:
+            await asyncio.sleep(random.randint(1, 10))
+            await get_bot().send_private_msg(user_id=fire_user_id, message="/echo 花花")  # 当未连接到onebot.v11协议端时会抛出异常
+            logger.info("发送火花")
+            sendSuccess = True
+        except ValueError as e:
+            logger.error("ValueError:{}", e)
+            logger.error("续火花插件获取bot失败，1s后重试")
+            await asyncio.sleep(1)  # 重试前时延，防止阻塞
+
+
+if scheduler:
+    scheduler.add_job(fire_scheduler, "cron", hour="0", id="fire_scheduler")
